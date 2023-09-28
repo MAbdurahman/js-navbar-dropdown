6c610cbcaf133f5a56db5f2590b1cf93908a6186
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-28 05:29:14 -0500

    Add preloader.gif with its styles and scripts

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+    // makes sure that whole site is loaded
+    $('#preloader-gif, #preloader').fadeOut(5000, function () {});
+});
+
+/*=============================================
+          js-simple-navbar scripts
+================================================*/
+$(function () {
+
+});
